136b0cb8822f36fbf9143507d957e352cc0d163a
AnicetChiza <chizaanicet@gmail.com>
2024-06-07 20:10:24 +0200

    add to my repository

--- gallery/asset/javascript/gallery.js
+++ gallery/asset/javascript/gallery.js
@@ -23,15 +23,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const showImages = document.querySelectorAll('.show-image');
-    const viewImages = document.querySelectorAll('.view-img');
+    const viewImages = document.querySelectorAll('.vieuw-img');
 
     viewImages.forEach(viewImage => {
         viewImage.addEventListener('click', function() {
-            const imageSrc = this.src; // Get the source of the clicked image
+            const imageSrc = this.src;
             showImages.forEach(showImage => {
                 const imgElement = showImage.querySelector('img');
-                imgElement.src = imageSrc; // Set the source of the image in show-image
-                showImage.style.display = 'flex'; // Display the show-image element
+                imgElement.src = imageSrc;
+                showImage.style.display = 'flex';
             });
         });
     });
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
     showImages.forEach(showImage => {
         const exitIcon = showImage.querySelector('.exit-icon');
         exitIcon.addEventListener('click', function() {
-            showImage.style.display = 'none'; // Hide the show-image element
+            showImage.style.display = 'none';
         });
     });
 });
