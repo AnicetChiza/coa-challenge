20855dc14eafc57f89286b6c299d8d5763548092
AnicetChiza <chizaanicet@gmail.com>
2024-06-07 18:15:36 +0200

    add to my repository

--- gallery/asset/javascript/gallery.js
+++ gallery/asset/javascript/gallery.js
@@ -1,17 +1,20 @@
-// Next and previous buttons moving
 document.addEventListener('DOMContentLoaded', () => {
-    const previousButton = document.querySelector('.previous');
-    const nextButton = document.querySelector('.next');
+    const previousButtons = document.querySelectorAll('.previous');
+    const nextButtons = document.querySelectorAll('.next');
 
-    if (previousButton) {
-        previousButton.addEventListener('click', () => {
-            window.location.href = 'gallery.html';
+    if (previousButtons.length > 0) {
+        previousButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                window.location.href = 'gallery.html';
+            });
         });
     }
 
-    if (nextButton) {
-        nextButton.addEventListener('click', () => {
-            window.location.href = 'gallerytwo.html';
+    if (nextButtons.length > 0) {
+        nextButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                window.location.href = 'gallerytwo.html';
+            });
         });
     }
 });
