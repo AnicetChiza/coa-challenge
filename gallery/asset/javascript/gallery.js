f76727f7ab9edd1f2ba7f9d184d720e15d90af30
AnicetChiza <chizaanicet@gmail.com>
2024-06-07 17:59:33 +0200

    add to my repository

--- gallery/asset/javascript/gallery.js
+++ gallery/asset/javascript/gallery.js
@@ -1,4 +1,17 @@
-setTimeout(function() {
-    const preloader = document.querySelector('.preloader');
-    preloader.style.display = 'none';
-}, 4000);
+// Next and previous buttons moving
+document.addEventListener('DOMContentLoaded', () => {
+    const previousButton = document.querySelector('.previous');
+    const nextButton = document.querySelector('.next');
+
+    if (previousButton) {
+        previousButton.addEventListener('click', () => {
+            window.location.href = 'gallery.html';
+        });
+    }
+
+    if (nextButton) {
+        nextButton.addEventListener('click', () => {
+            window.location.href = 'gallerytwo.html';
+        });
+    }
+});
